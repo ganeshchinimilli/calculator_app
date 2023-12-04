1773147cf5e27e6eb89a51153db36b98d81d3f6c
Ganesh <ganeshchinimilli4@gmail.com>
2023-12-04 22:07:42 +0530

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:calculator_app/calculator_view.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CalculatorView(),
+    );
+  }
+}
